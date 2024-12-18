be5d8fdc756dadbae9e6d00e7237ce9f4a2d1964
ppgrunheidt_meli <petra.pgrunheidt@mercadolivre.com>
2024-12-18 18:15:12 -0300

    adds update action for product

--- internal/models/products/product_repository.go
+++ internal/models/products/product_repository.go
@@ -4,5 +4,6 @@ type ProductRepository interface {
 	GetAll() (list []Product, err error)
 	GetById(id int) (prod Product, err error)
 	Create(prod ProductDTO) (newProd Product, err error)
+	Update(id int, prod ProductDTO) (updatedProd Product, err error)
 	Delete(id int) (err error)
 }
