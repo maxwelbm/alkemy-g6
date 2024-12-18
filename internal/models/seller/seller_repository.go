88f3646e22fa4fce65a1a74d34f4c2de1cf63674
Cleiton Ortega Dos Santos <cleiton.dsantos@mercadolivre.com>
2024-12-18 13:49:58 -0300

    Desenvolvimento do metodo delete seller

--- internal/models/seller/seller_repository.go
+++ internal/models/seller/seller_repository.go
@@ -3,4 +3,5 @@ package modelsSeller
 type SellerRepository interface {
 	GetAll() (sellerMap []Seller, err error)
 	GetById(id int) (sellerMap Seller, err error)
+	Delete(id int) (err error)
 }
--- internal/models/seller/seller_service.go
+++ internal/models/seller/seller_service.go
@@ -3,4 +3,5 @@ package modelsSeller
 type SellerService interface {
 	GetAll() (sellers []Seller, err error)
 	GetById(id int) (seller Seller, err error)
+	Delete(id int) (err error)
 }
