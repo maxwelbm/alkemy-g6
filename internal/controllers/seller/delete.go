f887c6ee0566554d9730d7bb13ab7eaf9de36884
mlvital_meli <maria.lvital@mercadolivre.com>
2024-12-19 17:15:36 -0300

    fix: implement validation of associations to sellers in the delete functionality

--- internal/controllers/seller/delete.go
+++ internal/controllers/seller/delete.go
@@ -1,10 +1,11 @@
 package sellerController
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
-
 	"github.com/go-chi/chi/v5"
+	"github.com/maxwelbm/alkemy-g6/internal/service"
 	"github.com/maxwelbm/alkemy-g6/pkg/response"
 )
 
@@ -18,13 +19,16 @@ func (controller *SellerDefault) Delete(w http.ResponseWriter, r *http.Request) 
 	}
 
 	_, err = controller.sv.GetById(id)
-
 	if err != nil {
 		response.Error(w, http.StatusNotFound, err.Error())
 		return
 	}
 
-	controller.sv.Delete(id)
+	err = controller.sv.Delete(id)
+	if errors.Is(err, service.ErrSellerServiceProductsAssociated) {
+		response.Error(w, http.StatusConflict, err.Error())
+		return
+	}
 
 	response.JSON(w, http.StatusNoContent, nil)
 
