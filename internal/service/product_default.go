bdaa94e052084ed4e69b31b0f47a53402d5b2403
ppgrunheidt_meli <petra.pgrunheidt@mercadolivre.com>
2024-12-20 09:02:21 -0300

    adds foreign key dependency error to product endpoints

--- internal/service/product_default.go
+++ internal/service/product_default.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"errors"
+
 	models "github.com/maxwelbm/alkemy-g6/internal/models/products"
 	repository "github.com/maxwelbm/alkemy-g6/internal/repository"
 )
@@ -24,11 +26,27 @@ func (s *ProductsDefault) GetById(id int) (prod models.Product, err error) {
 }
 
 func (s *ProductsDefault) Create(prod models.ProductDTO) (newProd models.Product, err error) {
+	// Check if the seller exists
+	if prod.SellerID != 0 {
+		if _, err = s.repo.SellersDB.GetById(prod.SellerID); err != nil {
+			err = errors.Join(errors.New("Seller not found"), err)
+			return
+		}
+	}
+
 	newProd, err = s.repo.ProductsDB.Create(prod)
 	return
 }
 
 func (s *ProductsDefault) Update(id int, prod models.ProductDTO) (updatedProd models.Product, err error) {
+	// Check if the seller exists
+	if prod.SellerID != 0 {
+		if _, err = s.repo.SellersDB.GetById(prod.SellerID); err != nil {
+			err = errors.Join(errors.New("Seller not found"), err)
+			return
+		}
+	}
+
 	updatedProd, err = s.repo.ProductsDB.Update(id, prod)
 	return
 }
