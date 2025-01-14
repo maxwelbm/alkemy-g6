f571b875612a5dfb971e47e49a82f6e9c74f51a5
Joao Pedro Ferreira <joao.mferreira@mercadolivre.com>
2025-01-14 10:49:02 -0300

    refactoring validate function of product-batches and fixing lint in create

--- internal/controllers/product_batches/product_batches_default.go
+++ internal/controllers/product_batches/product_batches_default.go
@@ -1,7 +1,6 @@
 package productbatchesctl
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/maxwelbm/alkemy-g6/internal/models"
@@ -49,55 +48,145 @@ type ProductBatchesResJSON struct {
 
 func (c *NewProductBatchesReqJSON) validateCreate() (err error) {
 	var validationErrors []string
+
 	var nilPointerErrors []string
 
-	if c.BatchNumber == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute BatchNumber cannot be nil")
-	} else if *c.BatchNumber == "" {
-		validationErrors = append(validationErrors, "error: attribute BatchNumber cannot be empty")
-	}
-	if c.InitialQuantity == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute InitialQuantity cannot be nil")
-	} else if *c.InitialQuantity <= 0 {
-		validationErrors = append(validationErrors, "error: attribute InitialQuantity cannot be negative")
-	}
-	if c.CurrentQuantity == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute CurrentQuantity cannot be nil")
-	} else if *c.CurrentQuantity <= 0 {
-		validationErrors = append(validationErrors, "error: attribute CurrentQuantity cannot be negative")
-	}
-	if c.CurrentTemperature == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute CurrentTemperature cannot be nil")
-	}
-	if c.MinimumTemperature == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute MinimumTemperature cannot be nil")
-	}
-	if c.DueDate == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute DueDate cannot be nil")
-	}
-	if c.ManufacturingDate == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute ManufacturingDate cannot be nil")
-	}
-	if c.ManufacturingHour == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute ManufacturingHour cannot be nil")
-	}
-	if c.ProductID == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute ProductID cannot be nil")
-	} else if *c.ProductID <= 0 {
-		validationErrors = append(validationErrors, "error: attribute ProductID must be positive")
-	}
-	if c.SectionID == nil {
-		nilPointerErrors = append(nilPointerErrors, "error: attribute SectionID cannot be nil")
-	} else if *c.SectionID <= 0 {
-		validationErrors = append(validationErrors, "error: attribute SectionID must be positive")
+	// Validate fields and append errors if any
+	if err := c.validateBatchNumber(&validationErrors, &nilPointerErrors); err != nil {
+		return err
 	}
 
+	if err := c.validateInitialQuantity(&validationErrors, &nilPointerErrors); err != nil {
+		return err
+	}
+
+	if err := c.validateCurrentQuantity(&validationErrors, &nilPointerErrors); err != nil {
+		return err
+	}
+
+	if err := c.validateCurrentTemperature(&nilPointerErrors); err != nil {
+		return err
+	}
+
+	if err := c.validateMinimumTemperature(&nilPointerErrors); err != nil {
+		return err
+	}
+
+	if err := c.validateDueDate(&nilPointerErrors); err != nil {
+		return err
+	}
+
+	if err := c.validateManufacturingDate(&nilPointerErrors); err != nil {
+		return err
+	}
+
+	if err := c.validateManufacturingHour(&nilPointerErrors); err != nil {
+		return err
+	}
+
+	if err := c.validateProductID(&validationErrors, &nilPointerErrors); err != nil {
+		return err
+	}
+
+	if err := c.validateSectionID(&validationErrors, &nilPointerErrors); err != nil {
+		return err
+	}
+
+	// Check if there are any accumulated errors
 	if len(nilPointerErrors) > 0 || len(validationErrors) > 0 {
-		var allErrors []string
-		allErrors = append(allErrors, nilPointerErrors...)
-		allErrors = append(allErrors, validationErrors...)
-
-		err = errors.New(fmt.Sprintf("validation errors: %v", allErrors))
+		allErrors := append(nilPointerErrors, validationErrors...)
+		return fmt.Errorf("validation errors: %v", allErrors)
 	}
+
+	return err
+}
+
+func (c *NewProductBatchesReqJSON) validateBatchNumber(validationErrors, nilPointerErrors *[]string) (err error) {
+	if c.BatchNumber == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute BatchNumber cannot be nil")
+	} else if *c.BatchNumber == "" {
+		*validationErrors = append(*validationErrors, "error: attribute BatchNumber cannot be empty")
+	}
+
+	return
+}
+
+func (c *NewProductBatchesReqJSON) validateInitialQuantity(validationErrors, nilPointerErrors *[]string) (err error) {
+	if c.InitialQuantity == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute InitialQuantity cannot be nil")
+	} else if *c.InitialQuantity <= 0 {
+		*validationErrors = append(*validationErrors, "error: attribute InitialQuantity must be positive")
+	}
+
+	return
+}
+
+func (c *NewProductBatchesReqJSON) validateCurrentQuantity(validationErrors, nilPointerErrors *[]string) (err error) {
+	if c.CurrentQuantity == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute CurrentQuantity cannot be nil")
+	} else if *c.CurrentQuantity <= 0 {
+		*validationErrors = append(*validationErrors, "error: attribute CurrentQuantity must be positive")
+	}
+
+	return
+}
+
+func (c *NewProductBatchesReqJSON) validateCurrentTemperature(nilPointerErrors *[]string) (err error) {
+	if c.CurrentTemperature == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute CurrentTemperature cannot be nil")
+	}
+
+	return
+}
+
+func (c *NewProductBatchesReqJSON) validateMinimumTemperature(nilPointerErrors *[]string) (err error) {
+	if c.MinimumTemperature == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute MinimumTemperature cannot be nil")
+	}
+
+	return
+}
+
+func (c *NewProductBatchesReqJSON) validateDueDate(nilPointerErrors *[]string) (err error) {
+	if c.DueDate == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute DueDate cannot be nil")
+	}
+
+	return
+}
+
+func (c *NewProductBatchesReqJSON) validateManufacturingDate(nilPointerErrors *[]string) (err error) {
+	if c.ManufacturingDate == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute ManufacturingDate cannot be nil")
+	}
+
+	return
+}
+
+func (c *NewProductBatchesReqJSON) validateManufacturingHour(nilPointerErrors *[]string) (err error) {
+	if c.ManufacturingHour == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute ManufacturingHour cannot be nil")
+	}
+
+	return
+}
+
+func (c *NewProductBatchesReqJSON) validateProductID(validationErrors, nilPointerErrors *[]string) (err error) {
+	if c.ProductID == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute ProductID cannot be nil")
+	} else if *c.ProductID <= 0 {
+		*validationErrors = append(*validationErrors, "error: attribute ProductID must be positive")
+	}
+
+	return
+}
+
+func (c *NewProductBatchesReqJSON) validateSectionID(validationErrors, nilPointerErrors *[]string) (err error) {
+	if c.SectionID == nil {
+		*nilPointerErrors = append(*nilPointerErrors, "error: attribute SectionID cannot be nil")
+	} else if *c.SectionID <= 0 {
+		*validationErrors = append(*validationErrors, "error: attribute SectionID must be positive")
+	}
+
 	return
 }
