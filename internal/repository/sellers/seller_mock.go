52dfc2c17d6d20414912a2cdbb3735a535aa1c0e
Cleiton Ortega Dos Santos <cleiton.dsantos@mercadolivre.com>
2025-01-21 15:39:53 -0300

    Service Tests

--- internal/repository/sellers/seller_mock.go
+++ internal/repository/sellers/seller_mock.go
@@ -23,7 +23,22 @@ func (m *SellerRepositoryMock) GetByID(id int) (models.Seller, error) {
 	return args.Get(0).(models.Seller), args.Error(1)
 }
 
-func (m *SellerRepositoryMock) GetByCID(CID int) (models.Seller, error) {
+func (m *SellerRepositoryMock) GetByCid(CID int) (models.Seller, error) {
 	args := m.Called(CID)
 	return args.Get(0).(models.Seller), args.Error(1)
+}
+
+func (m *SellerRepositoryMock) Create(seller models.SellerDTO) (models.Seller, error) {
+	args := m.Called(seller)
+	return args.Get(0).(models.Seller), args.Error(1)
+}
+
+func (m *SellerRepositoryMock) Update(id int, Seller models.SellerDTO) (SellerReturn models.Seller, err error) {
+	args := m.Called(id, Seller)
+	return args.Get(0).(models.Seller), args.Error(1)
+}
+
+func (m *SellerRepositoryMock) Delete(id int) error {
+	args := m.Called(id)
+	return args.Error(0)
 }
