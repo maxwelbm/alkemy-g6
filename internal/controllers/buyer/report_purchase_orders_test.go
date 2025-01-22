28585166b3f284359054cbf2187adb685dbf49c1
ppgrunheidt_meli <petra.pgrunheidt@mercadolivre.com>
2025-01-22 09:04:15 -0300

    adds tests for report purchase orders ctl

---
+++ internal/controllers/buyer/report_purchase_orders_test.go
@@ -0,0 +1,145 @@
+package buyersctl_test
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/maxwelbm/alkemy-g6/internal/controllers"
+	buyersctl "github.com/maxwelbm/alkemy-g6/internal/controllers/buyer"
+	"github.com/maxwelbm/alkemy-g6/internal/models"
+	"github.com/maxwelbm/alkemy-g6/internal/service"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReportPurchaseOrders(t *testing.T) {
+	type wanted struct {
+		calls      int
+		statusCode int
+		message    string
+	}
+	tests := []struct {
+		name    string
+		id      string
+		buyers  []models.BuyerPurchaseOrdersReport
+		callErr error
+		wanted  wanted
+	}{
+		{
+			name: "200 - When buyers are registered in the database and no id is passed",
+			buyers: []models.BuyerPurchaseOrdersReport{
+				{ID: 1, CardNumberID: "123456789", FirstName: "Buyer 1", LastName: "Ferreira", PurchaseOrdersCount: 1},
+				{ID: 2, CardNumberID: "987654321", FirstName: "Buyer 2", LastName: "Ferreira", PurchaseOrdersCount: 2},
+				{ID: 3, CardNumberID: "111111111", FirstName: "Buyer 3", LastName: "Ferreira", PurchaseOrdersCount: 3},
+			},
+			callErr: nil,
+			wanted: wanted{
+				calls:      1,
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name: "200 - When buyers are registered in the database and a valid id is passed",
+			buyers: []models.BuyerPurchaseOrdersReport{
+				{ID: 2, CardNumberID: "987654321", FirstName: "Buyer 2", LastName: "Ferreira", PurchaseOrdersCount: 2},
+			},
+			callErr: nil,
+			wanted: wanted{
+				calls:      1,
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name:    "200 - When no buyers are registered in the database and no id is passed",
+			id:      "2",
+			buyers:  []models.BuyerPurchaseOrdersReport{},
+			callErr: nil,
+			wanted: wanted{
+				calls:      1,
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name:    "400 - When passing a non numeric id",
+			id:      "S2",
+			callErr: nil,
+			wanted: wanted{
+				calls:      0,
+				statusCode: http.StatusBadRequest,
+				message:    "strconv.Atoi",
+			},
+		},
+		{
+			name:    "400 - When passing a negative id",
+			id:      "-1",
+			callErr: nil,
+			wanted: wanted{
+				calls:      0,
+				statusCode: http.StatusBadRequest,
+				message:    "Bad Request",
+			},
+		},
+		{
+			name:    "404 - When the respository raises a NotFound error",
+			id:      "999",
+			callErr: models.ErrBuyerNotFound,
+			wanted: wanted{
+				calls:      1,
+				statusCode: http.StatusNotFound,
+				message:    "buyer not found",
+			},
+		},
+		{
+			name:    "500 - When the repository returns an error",
+			buyers:  []models.BuyerPurchaseOrdersReport{},
+			callErr: errors.New("internal error"),
+			wanted: wanted{
+				calls:      1,
+				statusCode: http.StatusInternalServerError,
+				message:    "internal error",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arrange
+			sv := service.NewBuyersServiceMock()
+			ctl := controllers.NewBuyersController(sv)
+
+			url := "/api/v1/buyers/reportPurchaseOrders"
+			if tt.id != "" {
+				url += fmt.Sprintf("?id=%s", tt.id)
+			}
+			req := httptest.NewRequest(http.MethodGet, url, nil)
+			res := httptest.NewRecorder()
+
+			// Act
+			sv.On("ReportPurchaseOrders", mock.AnythingOfType("int")).Return(tt.buyers, tt.callErr)
+			ctl.ReportPurchaseOrders(res, req)
+
+			var decodedRes struct {
+				Message string                    `json:"message,omitempty"`
+				Data    []buyersctl.FullBuyerJSON `json:"data,omitempty"`
+			}
+			err := json.NewDecoder(res.Body).Decode(&decodedRes)
+
+			// Assert
+			sv.AssertNumberOfCalls(t, "ReportPurchaseOrders", tt.wanted.calls)
+			require.NoError(t, err)
+			require.Equal(t, tt.wanted.statusCode, res.Code)
+			if len(tt.buyers) > 0 {
+				for i, buyer := range tt.buyers {
+					require.Equal(t, buyer.FirstName, decodedRes.Data[i].FirstName)
+					require.Equal(t, buyer.LastName, decodedRes.Data[i].LastName)
+					require.Equal(t, buyer.CardNumberID, decodedRes.Data[i].CardNumberID)
+				}
+			}
+			require.Contains(t, decodedRes.Message, tt.wanted.message)
+		})
+	}
+}
