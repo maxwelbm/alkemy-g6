cb70ece8639d1b46c2b8bb0e9a313a50f705a83f
Petra <60860641+petragrunheidt@users.noreply.github.com>
2025-01-22 11:50:36 -0300

    Feat/buyers ctrl test get (#150)
    * adds mock to buyers service
    
    * Start of requirement 6
    
    * adds tests for get_all ctl
    
    * adds tests for get_by_id ctl
    
    * adds tests for report purchase orders ctl
    
    * pluralize buyers controllers folders (from buyer to buyers)
    
    ---------
    
    Co-authored-by: ppgrunheidt_meli <petra.pgrunheidt@mercadolivre.com>

--- internal/controllers/controllers_default.go
+++ internal/controllers/controllers_default.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-	buyersctl "github.com/maxwelbm/alkemy-g6/internal/controllers/buyer"
+	buyersctl "github.com/maxwelbm/alkemy-g6/internal/controllers/buyers"
 	carriesctl "github.com/maxwelbm/alkemy-g6/internal/controllers/carries"
 	employeesctl "github.com/maxwelbm/alkemy-g6/internal/controllers/employees"
 	inboundordersctl "github.com/maxwelbm/alkemy-g6/internal/controllers/inbound_orders"
--- internal/models/buyers.go
+++ internal/models/buyers.go
@@ -3,7 +3,7 @@ package models
 import "errors"
 
 var (
-	ErrBuyerNotFound = errors.New("Buyer not found ")
+	ErrBuyerNotFound = errors.New("buyer not found ")
 )
 
 type Buyer struct {
