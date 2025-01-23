68d4b22d869e3a549cbb9626faae182c09e31034
João Pedro Ferreira <42424338+joaopferreira98@users.noreply.github.com>
2025-01-23 09:43:58 -0300

    implementing tests for handler layer to delete  method (#169)
    * implementing tests for handler layer to delete  method
    
    * removing unnecessary error handling about no changes made reported in comments
    
    ---------
    
    Co-authored-by: Joao Pedro Ferreira <joao.mferreira@mercadolivre.com>

--- internal/repository/warehouses/delete.go
+++ internal/repository/warehouses/delete.go
@@ -3,22 +3,20 @@ package warehousesrp
 import "github.com/maxwelbm/alkemy-g6/internal/models"
 
 func (r *WarehouseRepository) Delete(id int) (err error) {
-	query := "DELETE FROM warehouses WHERE `id`=?"
-	result, err := r.db.Exec(query, id)
+	var exists bool
+	err = r.db.QueryRow("SELECT EXISTS(SELECT 1 FROM warehouses WHERE id = ?)", id).Scan(&exists)
 
 	if err != nil {
 		return
 	}
 
-	rowAffected, err := result.RowsAffected()
-	if err != nil {
-		return
-	}
-
-	if rowAffected == 0 {
+	if !exists {
 		err = models.ErrWareHouseNotFound
 		return
 	}
+
+	query := "DELETE FROM warehouses WHERE id = ?"
+	_, err = r.db.Exec(query, id)
 
 	return
 }
