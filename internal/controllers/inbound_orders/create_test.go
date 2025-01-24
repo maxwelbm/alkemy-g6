f35d1cdf78956299df1e57a8f9b9ba4adee5ae53
Cleiton Ortega Dos Santos <cleiton.dsantos@mercadolivre.com>
2025-01-24 10:54:16 -0300

    Development of inbound order tests

---
+++ internal/controllers/inbound_orders/create_test.go
@@ -0,0 +1,173 @@
+package inboundordersctl_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/maxwelbm/alkemy-g6/internal/controllers"
+	inboundordersctl "github.com/maxwelbm/alkemy-g6/internal/controllers/inbound_orders"
+	"github.com/maxwelbm/alkemy-g6/internal/models"
+	"github.com/maxwelbm/alkemy-g6/internal/service"
+	"github.com/maxwelbm/alkemy-g6/pkg/mysqlerr"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreate(t *testing.T) {
+	type wanted struct {
+		calls         int
+		statusCode    int
+		message       string
+		inboundOrders models.InboundOrders
+	}
+	tests := []struct {
+		name              string
+		inboundOrdersJSON string
+		callErr           error
+		wanted            wanted
+	}{
+		{
+			name: "201 - When the inbound is created successfully",
+			inboundOrdersJSON: `{
+				"id": 1,
+				"order_date": "2023-10-01",
+				"order_number": 12345,
+				"employee_id": 1,
+				"product_batch_id": 1,
+				"warehouse_id": 1
+			}`,
+			callErr: nil,
+			wanted: wanted{
+				calls:      1,
+				statusCode: http.StatusCreated,
+				message:    "Created",
+				inboundOrders: models.InboundOrders{
+					ID:             1,
+					OrderDate:      "2023-10-01",
+					OrderNumber:    12345,
+					EmployeeID:     1,
+					ProductBatchID: "1",
+					WarehouseID:    1,
+				},
+			},
+		},
+		{
+			name: "400 - When passing a body with invalid json",
+			inboundOrdersJSON: `{
+					"id": 1,
+					"order_date": "2023-10-01",
+					"order_number": 12345,
+					"employee_id": 1,
+					"product_batch_id": "invalid",
+					"warehouse_id": 1
+				}`,
+			callErr: nil,
+			wanted: wanted{
+				statusCode: http.StatusBadRequest,
+				message:    "json: cannot unmarshal",
+			},
+		},
+		{
+			name: "422 - When passing a body with a valid json with missing parameters",
+			inboundOrdersJSON: `{
+					"id": 1
+				}`,
+			callErr: nil,
+			wanted: wanted{
+				statusCode: http.StatusUnprocessableEntity,
+				message:    "error: order_date is required",
+			},
+		},
+		{
+			name: "409 - When the repository raises a DuplicateEntry error",
+			inboundOrdersJSON: `{
+					"id": 1,
+					"order_date": "2023-10-01",
+					"order_number": 12345,
+					"employee_id": 1,
+					"product_batch_id": 1,
+					"warehouse_id": 1
+				}`,
+			callErr: &mysql.MySQLError{Number: mysqlerr.CodeDuplicateEntry},
+			wanted: wanted{
+				calls:         1,
+				statusCode:    http.StatusConflict,
+				message:       "1062",
+				inboundOrders: models.InboundOrders{},
+			},
+		},
+		{
+			name: "409 - When the repository raises a CannotAddOrUpdateChildRow error",
+			inboundOrdersJSON: `{
+					"id": 1,
+					"order_date": "2023-10-01",
+					"order_number": 12345,
+					"employee_id": 1,
+					"product_batch_id": 1,
+					"warehouse_id": 1
+				}`,
+			callErr: &mysql.MySQLError{Number: mysqlerr.CodeCannotAddOrUpdateChildRow},
+			wanted: wanted{
+				calls:         1,
+				statusCode:    http.StatusConflict,
+				message:       "1452",
+				inboundOrders: models.InboundOrders{},
+			},
+		},
+		{
+			name: "500 - When the repository returns an unexpected error",
+			inboundOrdersJSON: `{
+					"id": 1,
+					"order_date": "2023-10-01",
+					"order_number": 12345,
+					"employee_id": 1,
+					"product_batch_id": 1,
+					"warehouse_id": 1
+				}`,
+			callErr: &mysql.MySQLError{Number: mysqlerr.CodeIncorrectDateValue},
+			wanted: wanted{
+				calls:         1,
+				statusCode:    http.StatusInternalServerError,
+				message:       "1292",
+				inboundOrders: models.InboundOrders{},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arrange
+			sv := service.NewInboundOrdersServiceMock()
+			ctl := controllers.NewInboundOrdersController(sv)
+
+			req := httptest.NewRequest(http.MethodPost, "/api/v1/inboundOrders", strings.NewReader(tt.inboundOrdersJSON))
+			res := httptest.NewRecorder()
+
+			// Act
+			sv.On("Create", mock.AnythingOfType("models.InboundOrdersDTO")).Return(tt.wanted.inboundOrders, tt.callErr)
+			ctl.Create(res, req)
+
+			// Assert
+			var decodedRes struct {
+				Message string                                   `json:"message,omitempty"`
+				Data    inboundordersctl.InboundOrdersAttributes `json:"data,omitempty"`
+			}
+			require.NoError(t, json.NewDecoder(res.Body).Decode(&decodedRes))
+
+			sv.AssertNumberOfCalls(t, "Create", tt.wanted.calls)
+			require.Equal(t, tt.wanted.statusCode, res.Code) /*
+				require.Contains(t, decodedRes.Message, tt.wanted.message)
+				if tt.wanted.statusCode == http.StatusCreated {
+					require.Equal(t, tt.wanted.inboundOrders.ID, decodedRes.Data.ID)
+					require.Equal(t, tt.wanted.inboundOrders.EmployeeID, decodedRes.Data.EmployeeID)
+					require.Equal(t, tt.wanted.inboundOrders.ProductBatchID, decodedRes.Data.ProductBatchID)
+					require.Equal(t, tt.wanted.inboundOrders.WarehouseID, decodedRes.Data.WarehouseID)
+				}
+				require.Contains(t, decodedRes.Message, tt.wanted.message)*/
+		})
+	}
+}
---
+++ internal/service/inbound_orders_default_test.go
@@ -0,0 +1,83 @@
+package service_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/maxwelbm/alkemy-g6/internal/models"
+	inboundOrdersrp "github.com/maxwelbm/alkemy-g6/internal/repository/inbound_orders"
+	"github.com/maxwelbm/alkemy-g6/internal/service"
+	"github.com/maxwelbm/alkemy-g6/pkg/mysqlerr"
+	"github.com/stretchr/testify/require"
+)
+
+var InboundOrdersFixture = []models.InboundOrders{
+	{
+		ID:             1,
+		OrderDate:      "2025-01-01",
+		OrderNumber:    100,
+		EmployeeID:     1,
+		ProductBatchID: "1",
+		WarehouseID:    1,
+	},
+	{
+		ID:             2,
+		OrderDate:      "2025-01-02",
+		OrderNumber:    101,
+		EmployeeID:     2,
+		ProductBatchID: "2",
+		WarehouseID:    2,
+	},
+}
+
+func TestInboundOrdersDefault_Create(t *testing.T) {
+	tests := []struct {
+		name                string
+		inboundOrder        models.InboundOrders
+		err                 error
+		wantedInboundOrders models.InboundOrders
+		wantedErr           error
+	}{
+		{
+			name:                "When the repository returns a inboundOrder",
+			inboundOrder:        InboundOrdersFixture[0],
+			err:                 nil,
+			wantedInboundOrders: InboundOrdersFixture[0],
+			wantedErr:           nil,
+		},
+		{
+			name:                "When the repository returns an error",
+			inboundOrder:        InboundOrdersFixture[0],
+			err:                 &mysql.MySQLError{Number: mysqlerr.CodeDuplicateEntry},
+			wantedInboundOrders: InboundOrdersFixture[0],
+			wantedErr:           &mysql.MySQLError{Number: mysqlerr.CodeDuplicateEntry},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Arrange
+			rp := inboundOrdersrp.NewInboundOrdersRepositoryMock()
+			sv := service.NewInboundOrdersService(rp)
+
+			product_batch_id, _ := strconv.Atoi(tt.inboundOrder.ProductBatchID)
+
+			dto := models.InboundOrdersDTO{
+				ID:             &tt.inboundOrder.ID,
+				OrderDate:      &tt.inboundOrder.OrderDate,
+				OrderNumber:    &tt.inboundOrder.OrderNumber,
+				EmployeeID:     &tt.inboundOrder.EmployeeID,
+				ProductBatchID: &product_batch_id,
+				WarehouseID:    &tt.inboundOrder.WarehouseID,
+			}
+			// Act
+			rp.On("Create", dto).Return(tt.inboundOrder, tt.err)
+			inboundOrder, err := sv.Create(dto)
+
+			// Assert
+			require.Equal(t, tt.wantedInboundOrders, inboundOrder)
+			require.Equal(t, tt.wantedErr, err)
+		})
+	}
+}
