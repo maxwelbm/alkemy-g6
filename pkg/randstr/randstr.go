5f124a2b73bcd669e6fce1ce0d9212cc58eb6396
ppgrunheidt_meli <petra.pgrunheidt@mercadolivre.com>
2025-02-13 12:57:35 -0300

    adds randstr package and applis random schema name to new db

---
+++ pkg/randstr/randstr.go
@@ -0,0 +1,30 @@
+package randstr
+
+import (
+	"math/rand"
+)
+
+var alphabet = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var alphanumeric = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func Chars(n int) string {
+	rand.New(rand.NewSource(1))
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+
+	return string(b)
+}
+
+func Alphanumeric(n int) string {
+	rand.New(rand.NewSource(1))
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = alphanumeric[rand.Intn(len(alphanumeric))]
+	}
+
+	return string(b)
+}
