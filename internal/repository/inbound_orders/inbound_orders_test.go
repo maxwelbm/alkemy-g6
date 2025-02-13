421437275a6f1b7e53b3acc3f2095bb2d3632853
ppgrunheidt_meli <petra.pgrunheidt@mercadolivre.com>
2025-02-13 09:33:46 -0300

    unifies seeds inserts

--- internal/repository/inbound_orders/inbound_orders_test.go
+++ internal/repository/inbound_orders/inbound_orders_test.go
@@ -12,43 +12,16 @@ import (
 
 func inboundOrdersSeeds(db *sql.DB) {
 	// Create dependencies
-	_, err := db.Exec("INSERT INTO warehouses (id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature) VALUES (1, '123 Main St', '555-1234', 'WH001', 100, -10.0)")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	_, err = db.Exec("INSERT INTO localities (locality_name, province_name, country_name) VALUES ('Locality1', 'Province1', 'Country1');")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	_, err = db.Exec("INSERT INTO sellers (cid, company_name, address, telephone, locality_id) VALUES (1, 'Company1', '123 Main St', '555-1234', 1);")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	_, err = db.Exec("INSERT INTO products (product_code, description, height, length, width, net_weight, expiration_rate, freezing_rate, recommended_freezing_temperature, product_type_id, seller_id) VALUES ('P001', 'Product 1', 10.0, 20.0, 30.0, 40.0, 0.1, 0.2, -10.0, 1, 1);")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	_, err = db.Exec("INSERT INTO employees (id, card_number_id, first_name, last_name, warehouse_id) VALUES (1, 'EMP001', 'John', 'Doe', 1);")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	_, err = db.Exec("INSERT INTO sections (id, section_number, current_temperature, minimum_temperature, current_capacity, minimum_capacity, maximum_capacity, warehouse_id, product_type_id) VALUES (1, 'SC-001', -10.0, -20.0, 50, 10, 100, 1, 1);")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	_, err = db.Exec("INSERT INTO product_batches (id, batch_number, initial_quantity, current_quantity, current_temperature, minimum_temperature, due_date, manufacturing_date, manufacturing_hour, product_id, section_id) VALUES (1, '1', 200, 100, -10, -20, '2023-12-31', '2023-01-01', '08:00:00', 1, 1)")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	// Create inbound orders
-	_, err = db.Exec("INSERT INTO inbound_orders (id, order_date, order_number, employee_id, product_batch_id, warehouse_id) VALUES (1, '2023-01-01', 1001, 1, 1, 1);")
+	_, err := db.Exec(`
+		INSERT INTO warehouses (id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature) VALUES (1, '123 Main St', '555-1234', 'WH001', 100, -10.0);
+		INSERT INTO localities (locality_name, province_name, country_name) VALUES ('Locality1', 'Province1', 'Country1');
+		INSERT INTO sellers (cid, company_name, address, telephone, locality_id) VALUES (1, 'Company1', '123 Main St', '555-1234', 1);
+		INSERT INTO products (product_code, description, height, length, width, net_weight, expiration_rate, freezing_rate, recommended_freezing_temperature, product_type_id, seller_id) VALUES ('P001', 'Product 1', 10.0, 20.0, 30.0, 40.0, 0.1, 0.2, -10.0, 1, 1);
+		INSERT INTO employees (id, card_number_id, first_name, last_name, warehouse_id) VALUES (1, 'EMP001', 'John', 'Doe', 1);
+		INSERT INTO sections (id, section_number, current_temperature, minimum_temperature, current_capacity, minimum_capacity, maximum_capacity, warehouse_id, product_type_id) VALUES (1, 'SC-001', -10.0, -20.0, 50, 10, 100, 1, 1);
+		INSERT INTO product_batches (id, batch_number, initial_quantity, current_quantity, current_temperature, minimum_temperature, due_date, manufacturing_date, manufacturing_hour, product_id, section_id) VALUES (1, '1', 200, 100, -10, -20, '2023-12-31', '2023-01-01', '08:00:00', 1, 1);
+		INSERT INTO inbound_orders (id, order_date, order_number, employee_id, product_batch_id, warehouse_id) VALUES (1, '2023-01-01', 1001, 1, 1, 1);
+	`)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
