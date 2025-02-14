f273d0fa56ae67b4be5ddf7afd73a238a6ce2b01
mlvital_meli <maria.lvital@mercadolivre.com>
2025-02-14 18:13:10 -0300

    implement test for product records repository

--- internal/repository/product_records/create_test.go
+++ internal/repository/product_records/create_test.go
@@ -1,104 +1,103 @@
 package productrecordsrp
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/go-sql-driver/mysql"
-// 	"github.com/maxwelbm/alkemy-g6/internal/factories"
-// 	"github.com/maxwelbm/alkemy-g6/internal/models"
-// 	"github.com/maxwelbm/alkemy-g6/pkg/mysqlerr"
-// 	"github.com/maxwelbm/alkemy-g6/pkg/testdb"
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/go-sql-driver/mysql"
+	"github.com/maxwelbm/alkemy-g6/internal/factories"
+	"github.com/maxwelbm/alkemy-g6/internal/models"
+	"github.com/maxwelbm/alkemy-g6/pkg/mysqlerr"
+	"github.com/maxwelbm/alkemy-g6/pkg/testdb"
+	"github.com/stretchr/testify/require"
+)
 
-// func TestCreate(t *testing.T) {
-// 	db, truncate, teardown := testdb.NewConn(t)
-// 	defer teardown()
-// 	factory := factories.NewProductRecordsFactory(db)
-// 	fixture := factory.Build(models.ProductRecord{ID: 1})
-// 	// fixture.LastUpdateDate += "T00:00:00Z"
-// 	// date := fixture.LastUpdateDate + "T00:00:00Z"
-// 	type arg struct {
-// 		dto models.ProductRecordDTO
-// 	}
-// 	type want struct {
-// 		productRecord models.ProductRecord
-// 		err           error
-// 	}
-// 	tests := []struct {
-// 		name  string
-// 		setup func()
-// 		arg
-// 		want
-// 	}{
-// 		{
-// 			name: "When successfully creating a new product record",
-// 			setup: func() {
-// 				_, err := factories.NewProductFactory(db).Create(models.Product{ID: 1})
-// 				require.NoError(t, err)
-// 			},
-// 			arg: arg{
-// 				dto: models.ProductRecordDTO{
-// 					LastUpdateDate: &fixture.LastUpdateDate,
-// 					PurchasePrice:  &fixture.PurchasePrice,
-// 					SalePrice:      &fixture.SalePrice,
-// 					ProductID:      &fixture.ProductID,
-// 				},
-// 			},
-// 			want: want{
-// 				productRecord: fixture,
-// 				err:           nil,
-// 			},
-// 		},
-// 		{
-// 			name: "Error - When creating a productRecord from a non-existent product",
-// 			arg: arg{
-// 				dto: models.ProductRecordDTO{
-// 					LastUpdateDate: &fixture.LastUpdateDate,
-// 					PurchasePrice:  &fixture.PurchasePrice,
-// 					SalePrice:      &fixture.SalePrice,
-// 					ProductID:      &fixture.ProductID,
-// 				},
-// 			},
-// 			want: want{
-// 				err: &mysql.MySQLError{Number: mysqlerr.CodeCannotAddOrUpdateChildRow},
-// 			},
-// 		},
-// 		{
-// 			name: "Error - When creating a duplicated product record",
-// 			setup: func() {
-// 				_, err := factory.Create(fixture)
-// 				require.NoError(t, err)
-// 			},
-// 			arg: arg{
-// 				dto: models.ProductRecordDTO{
-// 					LastUpdateDate: &fixture.LastUpdateDate,
-// 					PurchasePrice:  &fixture.PurchasePrice,
-// 					SalePrice:      &fixture.SalePrice,
-// 					ProductID:      &fixture.ProductID,
-// 				},
-// 			},
-// 			want: want{
-// 				err: &mysql.MySQLError{Number: mysqlerr.CodeDuplicateEntry},
-// 			},
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			// Setup
-// 			t.Cleanup(truncate)
-// 			if tt.setup != nil {
-// 				tt.setup()
-// 			}
-// 			// Arrange
-// 			rp := NewProductRecordsRepository(db)
-// 			// Act
-// 			got, err := rp.Create(tt.dto)
-// 			// Assert
-// 			if tt.err != nil {
-// 				require.ErrorIs(t, tt.err, err)
-// 			}
-// 			require.Equal(t, tt.want.productRecord, got)
-// 		})
-// 	}
-// }
+func TestCreate(t *testing.T) {
+	db, truncate, teardown := testdb.NewConn(t)
+	defer teardown()
+	factory := factories.NewProductRecordsFactory(db)
+	fixture := factory.Build(models.ProductRecord{ID: 1})
+
+	type arg struct {
+		dto models.ProductRecordDTO
+	}
+	type want struct {
+		productRecord models.ProductRecord
+		err           error
+	}
+	tests := []struct {
+		name  string
+		setup func()
+		arg
+		want
+	}{
+		{
+			name: "When successfully creating a new product record",
+			setup: func() {
+				_, err := factories.NewProductFactory(db).Create(models.Product{ID: 1})
+				require.NoError(t, err)
+			},
+			arg: arg{
+				dto: models.ProductRecordDTO{
+					LastUpdateDate: &fixture.LastUpdateDate,
+					PurchasePrice:  &fixture.PurchasePrice,
+					SalePrice:      &fixture.SalePrice,
+					ProductID:      &fixture.ProductID,
+				},
+			},
+			want: want{
+				productRecord: fixture,
+				err:           nil,
+			},
+		},
+		{
+			name: "Error - When creating a productRecord from a non-existent product",
+			arg: arg{
+				dto: models.ProductRecordDTO{
+					LastUpdateDate: &fixture.LastUpdateDate,
+					PurchasePrice:  &fixture.PurchasePrice,
+					SalePrice:      &fixture.SalePrice,
+					ProductID:      &fixture.ProductID,
+				},
+			},
+			want: want{
+				err: &mysql.MySQLError{Number: mysqlerr.CodeCannotAddOrUpdateChildRow},
+			},
+		},
+		{
+			name: "Error - When creating a duplicated product record",
+			setup: func() {
+				_, err := factory.Create(fixture)
+				require.NoError(t, err)
+			},
+			arg: arg{
+				dto: models.ProductRecordDTO{
+					LastUpdateDate: &fixture.LastUpdateDate,
+					PurchasePrice:  &fixture.PurchasePrice,
+					SalePrice:      &fixture.SalePrice,
+					ProductID:      &fixture.ProductID,
+				},
+			},
+			want: want{
+				err: &mysql.MySQLError{Number: mysqlerr.CodeDuplicateEntry},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Setup
+			t.Cleanup(truncate)
+			if tt.setup != nil {
+				tt.setup()
+			}
+			// Arrange
+			rp := NewProductRecordsRepository(db)
+			// Act
+			got, err := rp.Create(tt.dto)
+			// Assert
+			if tt.err != nil {
+				require.ErrorIs(t, tt.err, err)
+			}
+			require.Equal(t, tt.want.productRecord, got)
+		})
+	}
+}
