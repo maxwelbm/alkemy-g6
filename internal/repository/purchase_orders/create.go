d9d59edbf91d78bfccb22611d293ba29e8d57698
João Pedro Ferreira <42424338+joaopferreira98@users.noreply.github.com>
2025-02-17 09:08:17 -0300

    adding test for purchase_orders repo layer to Create method (#223)
    * adding test for purchase_orders repo layer to Create method
    
    * removing unnecessary log
    
    ---------
    
    Co-authored-by: Joao Pedro Ferreira <joao.mferreira@mercadolivre.com>

--- internal/repository/purchase_orders/create.go
+++ internal/repository/purchase_orders/create.go
@@ -5,11 +5,6 @@ import (
 )
 
 func (r *PurchaseOrdersRepository) Create(purchaseOrdersDTO models.PurchaseOrdersDTO) (po models.PurchaseOrders, err error) {
-	var exists bool
-	if err = validateOrderNumber(r, purchaseOrdersDTO.OrderNumber, exists); err != nil {
-		return
-	}
-
 	query := "INSERT INTO purchase_orders(`order_number`,`order_date`,`tracking_code`,`buyer_id`,`product_record_id`) VALUES(?, ?, ?, ?, ?)"
 
 	result, err := r.DB.Exec(query, purchaseOrdersDTO.OrderNumber, purchaseOrdersDTO.OrderDate, purchaseOrdersDTO.TrackingCode, purchaseOrdersDTO.BuyerID, purchaseOrdersDTO.ProductRecordID)
@@ -22,19 +17,8 @@ func (r *PurchaseOrdersRepository) Create(purchaseOrdersDTO models.PurchaseOrder
 		return
 	}
 
-	query = "SELECT `id`,`order_number`, DATE_FORMAT(`order_date`, '%d/%m/%Y') AS order_date ,`tracking_code`,`buyer_id`,`product_record_id` FROM purchase_orders WHERE `id`=?"
-	if err = r.DB.QueryRow(query, lastInsertID).Scan(&po.ID, &po.OrderNumber, &po.OrderDate, &po.TrackingCode, &po.BuyerID, &po.ProductRecordID); err != nil {
-		return
-	}
+	query = "SELECT `id`,`order_number`, DATE_FORMAT(`order_date`, '%Y-%m-%d') AS order_date ,`tracking_code`,`buyer_id`,`product_record_id` FROM purchase_orders WHERE `id`=?"
+	err = r.DB.QueryRow(query, lastInsertID).Scan(&po.ID, &po.OrderNumber, &po.OrderDate, &po.TrackingCode, &po.BuyerID, &po.ProductRecordID)
 
 	return
-}
-
-func validateOrderNumber(r *PurchaseOrdersRepository, orderNumber string, exists bool) (err error) {
-	query := "SELECT EXISTS(SELECT 1 FROM purchase_orders WHERE `order_number`=?)"
-	if err = r.DB.QueryRow(query, orderNumber).Scan(&exists); err != nil {
-		return
-	}
-
-	return nil
 }
